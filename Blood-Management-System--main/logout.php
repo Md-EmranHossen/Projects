0ef39ed2f459da631d97cb46299a6884daefc17e
Emran Hossen <92653363+emranhossen01@users.noreply.github.com>
2023-11-09 14:42:16 +0600

    Delete Blood-Management-System--main directory

--- Blood-Management-System--main/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$un=$_SESSION['un'];
-
-// log out korle then r back kaj korbena *************session destroy 
-session_destroy();              
-header('Location:index.php');
-?>
